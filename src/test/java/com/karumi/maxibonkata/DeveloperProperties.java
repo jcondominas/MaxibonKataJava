3117c4293e63058b3fdc2a3259972f70e6e92d2c
Jordi Condominas <jcondominas@gmail.com>
2017-03-25 15:13:05 +0100

    Fix code style

--- src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
+++ src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
@@ -4,14 +4,25 @@ import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @RunWith(JUnitQuickcheck.class) public class DeveloperProperties {
-  private KarumiHQs karumiHqs = new KarumiHQs(message-> {
-  });
+  private KarumiHQs karumiHqs;
+  private Chat chat;
+
+  @Before
+  public void init() {
+    chat = mock(Chat.class);
+    karumiHqs = new KarumiHQs(chat);
+  }
 
   @Property
   public void numberOfMaxibonsCannotBeNegative(String name, int numberOfMaxibons) {
@@ -33,7 +44,6 @@ import static org.junit.Assert.assertTrue;
   public void numberOfMaxibonsShouldBeAlwaysMoreThan2(@From(DevelopersGenerator.class) Developer developer) {
     karumiHqs.openFridge(developer);
     assertTrue(karumiHqs.getMaxibonsLeft() >= 2);
-    System.out.println(karumiHqs.getMaxibonsLeft());
   }
 
   @Property
@@ -41,7 +51,6 @@ import static org.junit.Assert.assertTrue;
   @From(KarumiesGenerator.class) Developer developer) {
     karumiHqs.openFridge(developer);
     assertTrue(karumiHqs.getMaxibonsLeft() >= 2);
-    System.out.println(karumiHqs.getMaxibonsLeft());
   }
 
   @Property
@@ -49,14 +58,25 @@ import static org.junit.Assert.assertTrue;
   KarumiesGenerator.class) Developer> developers) {
     karumiHqs.openFridge(developers);
     assertTrue(karumiHqs.getMaxibonsLeft() >= 2);
-    System.out.println(karumiHqs.getMaxibonsLeft());
   }
 
   @Property
   public void numberOfMaxibonsShouldBeAlwaysMoreThan2WhenListOfDevelopers(List<@From(
-  DevelopersGenerator.class) Developer> developerList) {
+  value = DevelopersGenerator.class, frequency = 3) Developer> developerList) {
     karumiHqs.openFridge(developerList);
     assertTrue(karumiHqs.getMaxibonsLeft() >= 2);
-    System.out.println(karumiHqs.getMaxibonsLeft());
+  }
+
+  @Property(trials = 1000)
+  public void developerShouldNotifyWhenMaxibonsLowerThanMIN(@From(HungryDevelopersGenerator.class)
+                                                              Developer developer) {
+    karumiHqs.openFridge(developer);
+    verify(chat).sendMessage(anyString());
+  }
+
+  @Property(trials = 1000)
+  public void developerShouldNotNotify(@From(NotSoHungryDevelopersGenerator.class) Developer developer) {
+    karumiHqs.openFridge(developer);
+    verify(chat, never()).sendMessage(anyString());
   }
 }
